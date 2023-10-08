f9296646ab59400c201a79102267211b37097d5f
ChronoKross <nathanblainegwyn@gmail.com>
2023-10-07 23:18:09 -0400

    employees
    messing with case

--- models/Employee.js
+++ models/Employee.js
@@ -14,6 +14,6 @@ const employeeSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const employee = mongoose.model("employee", employeeSchema);
+const employee = mongoose.model("Employee", employeeSchema);
 
 module.exports = employee;
